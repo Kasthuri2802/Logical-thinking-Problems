a460a7889ac4c5a065bc2c3e5b70abd8c2a122ab
Kasthuri2802 <kasthurimuthuraj2002@gmail.com>
2023-11-23 18:47:07 +0530

    commit message here

---
+++ ExerciseList1/# problem #5.py
@@ -0,0 +1,45 @@
+# problem #5
+# In your college Python is taught in 3 different departments by the same professor. 
+# For each dept, get the no of students stutying Python and their marks in the final exam 
+# Get the input as comma seperated string
+
+# Find the top 3 marks in each class
+# Find the top 3 marks if all classes are combined.
+# Find the avg mark of students with passing mark in each class and the classes combined.
+# Find which class has the best average mark and least number of failed students.
+
+noOfStd_Ece = int(input("Number of Students in ECE: ")) 
+EceMarks=[int(x) for x in input("Final Exam marks of ECE Students: ").split(',')]
+noOfStd_Cse = int(input("Number of Students in CSE: ")) 
+CseMarks=[int(x) for x in input("Final Exam marks of CSE Students: ").split(',')]
+noOfStd_IT= int(input("Number of Students in IT: ")) 
+ITMarks=[int(x) for x in input("Final Exam marks of IT Students: ").split(',')]
+
+# Find the top 3 marks in each class
+
+EceMarks.sort(reverse = True)
+CseMarks.sort(reverse = True)
+ITMarks.sort(reverse = True)
+print("Top 3 marks in ECE : ",end=" ")
+for x in range(3):
+    print(EceMarks[x],end=" ")
+print()
+print("Top 3 marks in CSE : ",end=" ")
+for x in range(3):
+    print(CseMarks[x],end=" ")
+print()
+print("Top 3 marks in IT : ",end=" ")
+for x in range(3):
+    print(ITMarks[x],end=" ")
+print()
+# Find the top 3 marks if all classes are combined.
+allClass = EceMarks + CseMarks + ITMarks
+print(allClass)
+allClass.sort(reverse=True)
+
+
+
+
+
+
+
---
+++ ExerciseList1/1. Pattern Problem.py
@@ -0,0 +1,20 @@
+# Problem #1
+# Generate the following output using for loop. Go until g.
+# a
+# aba
+# abacaba
+# abacabadabacaba
+# abacabadabacabaeabacabadabacaba
+
+# look at the output and find the pattern. Hint - add next letter in the alphabet in each row
+
+n=int(input("enter number of rows: "))
+m=0
+for i in range(n):
+    num=97
+    m=2*m+1
+    for j in range(m):
+        print(chr(num),end=" ")
+        num+=1
+    print()
+    

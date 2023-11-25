2f01f8c4e8eeb919b55b3f7e26834692b9daa831
Kasthuri2802 <kasthurimuthuraj2002@gmail.com>
2023-11-25 14:47:37 +0530

    Create # Problem #3.py

---
+++ DAY 1/# Problem #3.py
@@ -0,0 +1,21 @@
+# Problem #3
+# you have a list of student names. you have one list each for their marks in CS, Math and English. 
+# hard code the values. no need to get it as input
+# Pass mark is 50.
+# Grade A is, mark 90 or above
+# Grade B , 80 or above
+# Fail is < 50
+# Print the name of the students who got A in all subjects or atleast one A and the rest B.
+# Try to use only one if statement.
+
+studentsName = ["kasthuri","jothika","praveen","jegan","vishali","santhosh","madhu"]   #list of students name
+CSMarks = [98,38,20,80,90,85,90]         #list of CS marks
+CSGrade=[]
+MathsMarks = [92,85,95,70,56,40,54]      #list of MathsMarks
+MathsGrade=[]
+EnglishMarks = [89,56,34,37,80,67,50]    #list of EnglishMarks  
+EnglishGrade=[]  
+# for x in range(len(CSMarks)):
+
+
+
